2e0dc3d25d15656d06e732b06ab4f3b6fa1dd68c
Chase Brown <cbrown6@oakland.edu>
2026-02-13 21:01:06 -0500

    Backend is connected and running with AWS. The only available table right now is location.

---
+++ Backend/server.js
@@ -0,0 +1,46 @@
+// @ts-nocheck
+/* eslint-env node */
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:process.env.DB_PORT,
+});
+
+// Test connection
+app.get("/test-db", (req, res) => {
+  db.query("SELECT 1 AS ok", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Database connection failed" });
+    }
+    res.json({ message: "Database connected!", results });
+  });
+});
+
+// REAL DATA ROUTE
+app.get("/location", (req, res) => {
+  db.query("SELECT * FROM location", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Query failed" });
+    }
+    res.json(results);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
