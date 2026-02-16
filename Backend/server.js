fb27534867442e9d068b73c69fde92e6c1338f70
Chase Brown <cbrown6@oakland.edu>
2026-02-15 21:58:50 -0500

    Routed post table and added column

--- Backend/server.js
+++ Backend/server.js
@@ -140,6 +140,41 @@ app.post("/threads", (req, res) => {
   });
 });
 
+app.post("/userpins", (req, res) => {
+  const { pin_id, user_id, coordinates, visibility } = req.body;
+
+  const sql = `
+    INSERT INTO threads (ThreadsID, PostID, WhoCanPost)
+    VALUES (?, ?, ?)
+  `;
+
+  db.query(sql, [pin_id, post_id, who_can_post], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to create thread" });
+    }
+
+    res.json({ message: "Thread created", id: result.insertId });
+  });
+});
+
+app.post("/posts", (req, res) => {
+  const { creator, creation_date, status, content, likesdislikes } = req.body;
+
+  const sql = `
+    INSERT INTO posts (Creator, Creation_Date, Status, Replies, Content, likesdislikes )
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql, [creator, creation_date, status, content, likesdislikes], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to create post" });
+    }
+
+    res.json({ message: "post created", id: result.insertId });
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
