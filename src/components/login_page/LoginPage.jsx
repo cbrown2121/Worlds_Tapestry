24e5a8d1e0a1e42b3a5dd5b38a2cb5a983ad3be5
quietlycreeping <jorlando@oakland.edu>
2026-03-23 20:23:02 -0400

    Login Page Image
    I do have to tweak the image but I think a background image is more fun than white.

--- src/components/login_page/LoginPage.jsx
+++ src/components/login_page/LoginPage.jsx
@@ -1,13 +1,14 @@
 import { useState, useContext } from "react"
 import { UserContext } from "../../contexts/Context.jsx";
 import { LoginForm } from "../form_component/LoginForm.jsx";
+import "./LoginPage.css";
 
 
 const LoginPage = () => {
 
     return (
         <>
-            <div id="landing-page" className="main-content">
+            <div id="login-page" className="main-content">
                 <LoginForm/>
             </div>
         </>
